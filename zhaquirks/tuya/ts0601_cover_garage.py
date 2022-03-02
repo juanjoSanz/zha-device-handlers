e8736aa2991a594cc211ef165e03c7f906638272
juanjoSanz <juanjo.sanzmartin@gmail.com>
2022-03-02 20:19:39 +0100

    Create ts0601_cover_garage.py

---
+++ zhaquirks/tuya/ts0601_cover_garage.py
@@ -0,0 +1,112 @@
+from zigpy.profiles import zha
+from zigpy.zcl.clusters.general import Basic, Groups, Identify, OnOff, Ota, Scenes, Time
+
+from zhaquirks.const import (
+    DEVICE_TYPE,
+    ENDPOINTS,
+    INPUT_CLUSTERS,
+    MODELS_INFO,
+    OUTPUT_CLUSTERS,
+    PROFILE_ID,
+)
+from zhaquirks.tuya import (
+    TuyaManufacturerWindowCover,
+    TuyaManufCluster,
+    TuyaWindowCover,
+    TuyaWindowCoverControl,
+)
+
+from zhaquirks.tuya import TuyaSwitch
+from zhaquirks.tuya.mcu import MoesSwitchManufCluster, TuyaOnOff, TuyaOnOffManufCluster
+
+
+
+#class TuyaGarageSwitchTO(TuyaSwitch):
+class TuyaGarageOpener(TuyaWindowCover):
+    """Tuya garage door opener switch.
+        # "node_descriptor": "NodeDescriptor(logical_type=<LogicalType.Router: 1>, complex_descriptor_available=0, 
+        #                       user_descriptor_available=0, reserved=0, aps_flags=0, frequency_band=<FrequencyBand.Freq2400MHz: 8>, 
+        #                       mac_capability_flags=<MACCapabilityFlags.AllocateAddress|RxOnWhenIdle|MainsPowered|FullFunctionDevice: 142>, 
+        #                       manufacturer_code=4417, maximum_buffer_size=66, maximum_incoming_transfer_size=66, server_mask=10752, 
+        #                       maximum_outgoing_transfer_size=66, descriptor_capability_field=<DescriptorCapability.NONE: 0>, 
+        #                       *allocate_address=True, *is_alternate_pan_coordinator=False, *is_coordinator=False, *is_end_device=False, 
+        #                       *is_full_function_device=True, *is_mains_powered=True, *is_receiver_on_when_idle=True, *is_router=True, 
+        #                       *is_security_capable=False)",
+        # {
+        #   "node_descriptor": "NodeDescriptor(logical_type=<LogicalType.Router: 1>, complex_descriptor_available=0, user_descriptor_available=0, reserved=0, aps_flags=0, frequency_band=<FrequencyBand.Freq2400MHz: 8>, mac_capability_flags=<MACCapabilityFlags.AllocateAddress|RxOnWhenIdle|MainsPowered|FullFunctionDevice: 142>, manufacturer_code=4417, maximum_buffer_size=66, maximum_incoming_transfer_size=66, server_mask=10752, maximum_outgoing_transfer_size=66, descriptor_capability_field=<DescriptorCapability.NONE: 0>, *allocate_address=True, *is_alternate_pan_coordinator=False, *is_coordinator=False, *is_end_device=False, *is_full_function_device=True, *is_mains_powered=True, *is_receiver_on_when_idle=True, *is_router=True, *is_security_capable=False)",
+        #   "endpoints": {
+        #     "1": {
+        #       "profile_id": 260,          # 0x0104
+        #       "device_type": "0x0051",
+        #       "in_clusters": [
+        #         "0x0000",             # Basic (0x0000)
+        #         "0x0004",             
+        #         "0x0005",
+        #         "0xef00"
+        #       ],
+        #       "out_clusters": [
+        #         "0x000a",
+        #         "0x0019"              # OTA (0x0019)
+        #       ]
+        #     },
+        #     "242": {
+        #       "profile_id": 41440,
+        #       "device_type": "0x0061",
+        #       "in_clusters": [],
+        #       "out_clusters": [
+        #         "0x0021"
+        #       ]
+        #     }
+        #   },
+        #   "manufacturer": "_TZE200_nklqjk62",
+        #   "model": "TS0601",
+        #   "class": "zigpy.device.Device"
+        # }
+    """
+    signature = { 
+        MODELS_INFO: [
+            ("_TZE200_nklqjk62", "TS0601"),
+        ],
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.SMART_PLUG,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    Time.cluster_id,
+                    TuyaOnOffManufCluster.cluster_id
+                ],
+                OUTPUT_CLUSTERS: [Time.cluster_id, Ota.cluster_id],
+            },
+            242: {
+                PROFILE_ID: 0xA1E0,
+                DEVICE_TYPE: 0x0061,
+                INPUT_CLUSTERS: [],
+                OUTPUT_CLUSTERS: [0x0021],
+            },
+        },
+    }
+    
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_LIGHT,
+                INPUT_CLUSTERS: [
+                     Basic.cluster_id,
+                    Groups.cluster_id,
+                    Scenes.cluster_id,
+                    MoesSwitchManufCluster,
+                    TuyaOnOff,
+                ],
+                OUTPUT_CLUSTERS: [Time.cluster_id, Ota.cluster_id],
+            },
+            242: {
+                PROFILE_ID: 0xA1E0,
+                DEVICE_TYPE: 0x0061,
+                INPUT_CLUSTERS: [],
+                OUTPUT_CLUSTERS: [0x0021],
+            },
+        },
+    }
